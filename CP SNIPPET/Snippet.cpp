4851bc584b2a0a0859ca684c25a42809999719ad
Sameep Chaurasia <chaurasiasameep26@gmail.com>
2023-05-07 19:44:30 +0530

    Add files via upload

---
+++ CP SNIPPET/Snippet.cpp
@@ -0,0 +1,51 @@
+                                                         /*  ॐ  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define read(type) readInt<type>() // Fast read
+#define ll long long
+#define endl "\n"
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+/*
+███╗░░░███╗░█████╗░██╗███╗░░██╗        ░█████╗░░█████╗░██████╗░███████╗
+████╗░████║██╔══██╗██║████╗░██║        ██╔══██╗██╔══██╗██╔══██╗██╔════╝
+██╔████╔██║███████║██║██╔██╗██║        ██║░░╚═╝██║░░██║██║░░██║█████╗░░
+██║╚██╔╝██║██╔══██║██║██║╚████║        ██║░░██╗██║░░██║██║░░██║██╔══╝░░
+██║░╚═╝░██║██║░░██║██║██║░╚███║        ╚█████╔╝╚█████╔╝██████╔╝███████╗
+╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚══╝        ░╚════╝░░╚════╝░╚═════╝░╚══════╝
+*/
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int ttt; cin >> ttt;
+    while(ttt--) 
+    {
+
+        
+
+    }
+    return 0;
+}
+
+//By-SAMEEP CHAURASIA
